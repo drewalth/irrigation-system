d23d21dad891066f63634911b364233648ee99d9
Drew <andrew.althage@gmail.com>
2021-03-23 20:26:17 -0600

    more ui wip

---
+++ pages/api/rain.ts
@@ -0,0 +1,12 @@
+const {NODE_ENV} = process.env
+
+export default async (req, res) => {
+
+    if(NODE_ENV === 'development') {
+        await new Promise(resolve => setTimeout(resolve, 3000))
+        res.status(200).json([1])
+        return
+    }
+
+    res.status(200).json([1])
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,122 @@
-import { useState, useEffect } from 'react'
-import { Layout, Switch, List, Typography, Row, Col } from 'antd'
+import {useState, useEffect} from 'react'
+import {Layout, Switch, Card, Typography, Row, Col, Spin, Button, Calendar} from 'antd'
 import 'antd/dist/antd.css';
-const {Header, Footer, Content} = Layout
+
+const {Header, Content} = Layout
+
 interface IData {
-  pong: string
+    pong: string
 }
 
 export default function Home() {
 
-  const [loading, setLoading] = useState(false)
-  const [soilReadings, setSoilReading] = useState([])
-  const [soilReadingsLoading, setSoilReadingsLoading] = useState(false)
-  const [data, setData] = useState<IData>({
-    pong: ''
-  })
-  const [error, setError] = useState()
-
-  const toggleValve = (active: boolean) => {
-    fetch(`/api/valve?open=${active}`).then(res => res.json()).then(data => setData(data))
-  }
-
-  const getSoilReadings = () => {
-    setSoilReadingsLoading(true)
-    fetch('/api/soil').then(res => res.json()).then(data => setSoilReading(data)).finally(() => setSoilReadingsLoading(false))
-  }
-
-  const soilReadingOptimal = (reading:number) => {
-    return (reading === 0)
-  }
-
-  const initializeSystem = () => {
-    fetch('/api/init')
-  }
-
-  useEffect(() => {
-    initializeSystem()
-    getSoilReadings()
-  }, [])
+    const [loading, setLoading] = useState(false)
+    const [soilReadings, setSoilReading] = useState([])
+    const [soilReadingsLoading, setSoilReadingsLoading] = useState(false)
+    const [rainReadingsLoading, setRainReadingsLoading] = useState(false)
+    const [rainReadings, setRainReadings] = useState([])
 
 
-  return (
-    <>
-      <Layout>
-        <Header>
-          Header
-        </Header>
-        <Content>
-          <Row>
-            <Col span={14} offset={4}>
-              <div>
-                <label htmlFor="switch" style={{marginRight: '8px'}}>Valve Active</label>
-                <Switch loading={loading} onChange={toggleValve} />
-              </div>
-              <List bordered
-                    dataSource={soilReadings}
-                    header={<Typography.Title level={3}>Soil Sensors</Typography.Title> }
-                    renderItem={(item,index) => <List.Item>Sensor {index + 1} {soilReadingOptimal(item) ? 'good' : 'bad'}</List.Item>}
-              />
-            </Col>
-          </Row>
-        </Content>
-      </Layout>
-    </>
+    const [data, setData] = useState<IData>({
+        pong: ''
+    })
 
-  )
+    const toggleValve = (active: boolean) => {
+        fetch(`/api/valve?open=${active}`).then(res => res.json()).then(data => setData(data))
+    }
+
+    const getSoilReadings = () => {
+        setSoilReadingsLoading(true)
+        fetch('/api/soil').then(res => res.json()).then(data => setSoilReading(data)).finally(() => setSoilReadingsLoading(false))
+    }
+
+    const getRainReadings = () => {
+        setRainReadingsLoading(true)
+        fetch('/api/rain').then(res => res.json()).then(data => setRainReadings(data)).finally(() => setRainReadingsLoading(false))
+    }
+
+    const readingOptimal = (reading: number): string => {
+        return (reading === 0) ? 'reading meets standards' : 'reading does not meet standards'
+    }
+
+    const initializeSystem = () => {
+        fetch('/api/init')
+    }
+
+    useEffect(() => {
+        initializeSystem()
+        getRainReadings()
+        getSoilReadings()
+    }, [])
+
+    const rowStyle = {marginBottom: '2rem'}
+
+    return (
+        <Layout>
+            <Header style={{display: 'flex', alignItems: 'flex-end'}}>
+                <Typography.Title mark level={2}>Irrigation System</Typography.Title>
+            </Header>
+            <Content style={{height:'100vh'}}>
+                <Row style={{...rowStyle, marginTop: '2rem'}}>
+                    <Col span={14} offset={4}>
+
+                    </Col>
+                </Row>
+                <Row style={rowStyle}>
+                    <Col span={14} offset={4}>
+                        <label htmlFor="switch" style={{marginRight: '8px'}}>Valve Open (Watering)</label>
+                        <Switch loading={loading} onChange={toggleValve}/>
+                    </Col>
+                </Row>
+                <Row style={rowStyle}>
+                    <Col span={14} offset={4}>
+                        <Card title="Soil Sensors" extra={<Button onClick={getSoilReadings} disabled={soilReadingsLoading}>Refresh</Button>}>
+                            {soilReadingsLoading && (
+                                <div style={{display: 'flex'}}>
+                                    <Spin/>
+                                    <span style={{marginLeft: '1rem'}}>
+                                            Loading Soil Moisture Readings
+                                        </span>
+                                </div>
+                            )}
+                            {!soilReadingsLoading && soilReadings.length ? (
+                                <ul>
+                                    {soilReadings.map((reading, index) => (
+                                        <li key={String(reading + index)}>Sensor {index + 1}: {readingOptimal(reading)}</li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                // this feels like bad practice
+                                <></>
+                            )}
+                        </Card>
+                    </Col>
+                </Row>
+                <Row style={rowStyle}>
+                    <Col span={14} offset={4}>
+                        <Card title="Rain Sensors" extra={<Button onClick={getRainReadings} disabled={rainReadingsLoading}>Refresh</Button>}>
+                            {rainReadingsLoading && (
+                                <div style={{display: 'flex'}}>
+                                    <Spin/>
+                                    <span style={{marginLeft: '1rem'}}>
+                                            Loading Soil Moisture Readings
+                                    </span>
+                                </div>
+                            )}
+                            {!rainReadingsLoading && rainReadings.length ? (
+                                <ul>
+                                    {rainReadings.map((reading, index) => (
+                                        <li key={String(reading + index)}>Sensor {index + 1}: {readingOptimal(reading)}</li>
+                                    ))}
+                                </ul>
+                            ) : (
+                                // this feels like bad practice
+                                <></>
+                            )}
+                        </Card>
+                    </Col>
+                </Row>
+            </Content>
+        </Layout>
+    )
 }
