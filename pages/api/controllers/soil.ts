ab21d379fdc7d9285890cc8c7e4b610bdaec63cd
Drew <andrew.althage@gmail.com>
2021-04-20 09:19:12 -0600

    soil sensor polling

--- pages/api/controllers/soil.ts
+++ pages/api/controllers/soil.ts
@@ -1,20 +1,13 @@
 import rpio from 'rpio'
 const activePins = [22, 36, 33]
 
-const openPins = async () => {
-  await Promise.all(activePins.map((p) => rpio.open(p, rpio.INPUT)))
-  return console.log('pins opened')
-}
-
 rpio.init({
   mock: process.env.NODE_ENV === 'development' ? 'raspi-3' : undefined,
 })
 
-openPins()
-
-const { NODE_ENV } = process.env
-
 export const getSoilReadings = async (): Promise<any> => {
+  await Promise.all(activePins.map((p) => rpio.open(p, rpio.INPUT)))
+
   return Promise.all(activePins.map((pin) => rpio.read(pin)))
     .then((res) => res)
     .catch((e) => console.error(e))
